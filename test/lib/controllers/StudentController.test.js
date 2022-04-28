16d17779982a4dcc3513c5ff72b837869ec1c436
Sergio Lopez <sergio@softnya.com>
2022-04-27 21:06:38 -0500

    Mejora de pruebas de unidad de validacion de datos

--- test/lib/controllers/StudentController.test.js
+++ test/lib/controllers/StudentController.test.js
@@ -3,12 +3,16 @@ const StudentController = require("../../../lib/controllers/StudentController");
 describe("StudentController", () => {
     test("Debe extraer todos los estudiantes", () => {
         const students = StudentController.getAllStudents();
-        expect(students.length).toBe(51);
+        expect(students.length > 0).toBe(true);
     });
 
     test("Debe filtrar los estudiantes que tienen certificación", () => {
-        const students = StudentController.getAllStudents();
-        const data = StudentController.getStudentsEmailWithCertification(students);
-        expect(data.length).toBe(29);
+        const data = StudentController.getStudentsEmailWithCertification();
+        expect(data.map(student => student.email).length > 0).toBe(true);
+    });
+
+    test("Debe filtrar los estudiantes que tienen más de 500 créditos", () => {
+        const data = StudentController.getStudentsWithCredits(500);
+        expect(data.filter(student => student.credits >= 500).length > 0).toBe(true);
     });
 });
--- test/lib/services/StudentService.test.js
+++ test/lib/services/StudentService.test.js
@@ -5,12 +5,12 @@ describe("StudentService", () => {
     test("Filtrar estudiantes con certificacion", () => {
         const students = StudentController.getAllStudents();
         const data = StudentService.filterByCertification(students);
-        expect(data.length > 0 && data[0].haveCertification == true).toBe(true);
+        expect(data.filter(student => student.haveCertification === true).length > 0).toBe(true);
     });
 
     test("Filtrar estudiantes con creditos", () => {
         const students = StudentController.getAllStudents();
         const data = StudentService.filterByCredits(students, 500);
-        expect(data.length > 0 && data[0].credits >= 500).toBe(true);
+        expect(data.filter(student => student.credits >= 500).length > 0).toBe(true);
     });
 });
